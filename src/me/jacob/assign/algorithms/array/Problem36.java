2bcd911eb96503a1ebab56a49153d1864a6e7f67
DeltaOrion <specdartpokeman@gmail.com>
2023-06-04 12:21:03 +1000

    P36 docs

--- src/me/jacob/assign/algorithms/array/Problem36.java
+++ src/me/jacob/assign/algorithms/array/Problem36.java
@@ -8,6 +8,17 @@ public class Problem36 {
         p.isValidSudoku(boards);
     }
 
+    /**
+     * Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+     *
+     * Each row must contain the digits 1-9 without repetition.
+     * Each column must contain the digits 1-9 without repetition.
+     * Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+     * Note:
+     *
+     * A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+     * Only the filled cells need to be validated according to the mentioned rules.
+     */
     public boolean isValidSudoku(char[][] board) {
         boolean[][] cellsValid = new boolean[board.length][board.length];
         boolean[][] colsValid = new boolean[board.length][board.length];
@@ -18,15 +29,18 @@ public class Problem36 {
                 if(board[i][j] == '.')
                     continue;
 
+                //check row
                 int x = board[i][j] - '1';
                 if(rowValid[x])
                     return false;
 
                 rowValid[x] = true;
+                //check columns
                 if(colsValid[j][x])
                     return false;
 
                 colsValid[j][x] = true;
+                //check 3x3 cells
                 int cell = (i / 3) * 3 + j / 3;
                 if(cellsValid[cell][x])
                     return false;
